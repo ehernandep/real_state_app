6b78a3e85136b92a7597728bec4584c13bb4fb0c
ehernandep <65094614+ehernandep@users.noreply.github.com>
2022-09-04 19:09:59 -0500

    profiles:245

---
+++ estate-src/apps/profiles/exceptions.py
@@ -0,0 +1,8 @@
+from rest_framework.exceptions import APIException 
+
+class ProfileNotFound(APIException):
+  status_code=404
+  default_detail="the requested profile does not exist"
+class NotYourProfile(APIException):
+  status_code=403
+  default_detail="you can't edit a profile that doesn't belong to you"
